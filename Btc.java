73653409c4e671a11c0832316ce91554e139dcbe
kfalez <kelten.falez@ucalgary.ca>
2021-07-10 13:35:50 -0600

    Debugging

--- Btc.java
+++ Btc.java
@@ -1,34 +1,60 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Btc {
-	
-	private double currentPrice;
-	private double amountToMine;
+
+	private String currentPrice;
+	private static double bitcoins;
 	private double timeToMine;
 	private double rate;
+	public static double sumRates = 0;
+	public static ArrayList<String> miners = new ArrayList<String>();
+	public static ArrayList<Integer> wattage = new ArrayList<Integer>();
+	public static ArrayList<Double> rates = new ArrayList<Double>();
+
+	public static double time;
 	
-	public void getPrice() {
-		currentPrice = 31000.03;
+	public String getPrice() {
+		String result = "[" + CoindeskConnection.connect("https://api.coindesk.com/v1/bpi/currentprice.json")+"]";
+		return currentPrice = CoindeskConnection.parseBitcoin(result);
 	}
 	
 	public double setAmount(double amount) {
-		amountToMine = amount;
+		bitcoins = amount;
 		return amount;
 	}
 	
-	public double setRate() {
-		rate = 0.00010294;
-		return rate;
+	
+	public final static double getTime() {
+		try {
+			Scanner s = new Scanner(new File("MiningSetup.csv"), "UTF-8");
+			while (s.hasNextLine()) {
+				String line = s.nextLine();
+				miners.add(line.trim().split(",")[0]);
+				rates.add(Double.parseDouble(line.trim().split(",")[1]));
+				wattage.add(Integer.parseInt(line.trim().split(",")[2]));
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		for(int i = 0; i < rates.size(); i++)
+		    sumRates += rates.get(i);
+		return time = (bitcoins/sumRates)/24;
 	}
 	
-	
-	public double getTime() {
-		timeToMine = (amountToMine / rate) / 24;
-		return timeToMine;
-	}
 
 	
 	public String toString() {
 		
-		 return "\nIt will take: " + getTime() + " days to mine " + amountToMine + " BTC." ;
+		 return "\nIt will take: " + getTime() + " days to mine " + bitcoins + " BTC." ;
 }
+	
+	public String testToString() {
+		return getPrice();
+	}
 }
+
